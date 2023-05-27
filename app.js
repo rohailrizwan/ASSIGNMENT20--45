73ad76110be860aea3d153d0bbfe3a3d52e17a78
rohail <rupani.rohail@gmail.com>
2023-05-27 09:21:59 +0500

    message

---
+++ app.js
@@ -0,0 +1,84 @@
+// var allLower = userInput.toLowerCase();
+
+// var x="ROHAIL"
+// x=x.toLowerCase()
+// console.log(x)
+
+// var x="rohail"
+// x=x.toUpperCase()
+// console.log(x)
+
+// var x="LEARNING JAVASCRIPT"
+// var learn=x.toLowerCase()
+// console.log(learn)
+
+// var arr=["ALI","SALMAN"]
+// var x=arr[0].toLowerCase()
+// console.log(x)
+
+// var x="rohail"
+// x=x.toUpperCase()
+// alert(x)
+
+// var cityName = "kaRacHi";
+// var x=cityName.slice(0,1).toUpperCase() + cityName.slice(1).toLowerCase()
+// console.log(x)
+
+// chap 22 to 25
+
+// var sameword="captain"
+// var c=sameword.slice(1,3)
+// console.log(c)
+
+// var x="LEARNING JAVASCRIPT"
+// var length=x.length
+// console.log(length)
+
+// var str="elephant"
+// var midChar=str.slice(2,6)
+// console.log(midChar)
+
+// var myString = "Pakistan";
+// var characterCount = myString.length;
+// console.log(characterCount);
+
+// var myString = "Pakistan";
+// var l=myString.length
+// console.log(l)
+// var F_char=myString.slice(0,1)+myString.slice(-3)
+// console.log(F_char)
+
+// var text = "Be yourself Live simply";
+// var indx = text.indexOf("Live");
+// console.log(indx)
+// var text = "Be yourself Live simply live";
+// var indx = text.lastIndexOf("live");
+// console.log(indx)
+
+// var text="last instance go Live simply"
+// var indx=text.indexOf("go")
+// console.log(indx)
+
+// if (myString.charAt(indexNum) !== "")
+
+// var str="abcde"
+// str=str.charAt(2)
+// console.log(str)
+
+// var text="last instance go Live simply"
+//  text=text.charAt(10)
+// console.log(text)
+
+// var text="last instance go Live simply"
+//  x=text.charAt(text.length-1)
+// console.log(x)
+
+// var text=prompt(" ")
+//  char=text.charAt(5)
+// console.log(char)
+
+// var text="last instance go Live simply"
+// if(text.charAt(3) == "t"){
+//     console.log(text.charAt(3))
+// }
+
